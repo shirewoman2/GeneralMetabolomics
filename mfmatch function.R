a42843e5204c0e389a221c5512df092a8f6c8f1c
Laura Shireman <shirewoman2@gmail.com>
2015-02-12 14:11:43 -0800

    In process of fixing the mfmatch function to remove isomers and still work completely with MFs with missing RTs.

--- mfmatch function.R
+++ mfmatch function.R
@@ -35,15 +35,17 @@ mfmatch <- function(X, Y, PPM = 15, RTRange = 0.2){
       # can't determine which would be the best match without RT
       if (anyNA(DF.Y$RT)) {
             
-            Isomers <<- dlply(DF.Y[is.na(DF.Y$RT), ], "mz")
+            DF.Y$mz.round <- round(DF.Y$mz, digits = 4)
+            
+            Isomers <<- dlply(DF.Y[is.na(DF.Y$RT), ], "mz.round")
             
             for (i in 1:length(Isomers)){
                   Isomers[[i]]$MassFeature[1] <- str_c(Isomers[[i]]$MassFeature, 
-                              collapse = " ")
+                                                       collapse = " ")
                   Isomers[[i]] <- Isomers[[i]][1, ]
             }
-                  
-            DF.Y <- rbind.fill(Isomers)
+            
+            DF.Y <- rbind.fill(Isomers, DF.Y[complete.cases(DF.Y$RT), ])
       }
       
       # Renaming to keep track of which mass feature, m/z, and RT came from 
@@ -87,81 +89,82 @@ mfmatch <- function(X, Y, PPM = 15, RTRange = 0.2){
                              & is.na(DF.Y$RT.Y), ]
                   
             }
+            Matched.Y[i] <- as.numeric(nrow(MFmatch[[i]]))            
       }
-      Matched.Y[i] <- as.numeric(nrow(MFmatch[[i]]))
-}
 
-# Making a new data.frame to hold matched mass features
-Matches <- data.frame(MassFeature.X = DF.X$MassFeature.X,
-                      MassFeature.Y = NA, 
-                      mz.X = DF.X$mz.X, 
-                      mz.Y = NA,
-                      RT.X = DF.X$RT.X,
-                      RT.Y = NA,
-                      NumMatched = Matched.Y,
-                      ppm = NA, 
-                      RTdif = NA)
-
-Matches <- dlply(Matches, "MassFeature.X")
-
-# If there was more than 1 potential match and the RT was listed, take 
-# the one that was closest by m/z and then by RT. If there was no RT 
-# listed, take all matches. 
-# Calculate the difference in m/z in ppm and RT in min.
-for (i in which(Matched.Y > 0)){
       
-      if (is.na(Matches[[i]]$RT.X)){
-            Matches[[i]] <- MFmatch[[i]]
-            Matches[[i]]$ppm <- 
-                  abs((mz.X[i] - Matches[[i]]$mz.Y)/mz.X[i]*1e6)
-            Matches[[i]]$MassFeature.X <- MFname.X[i]
+      
+      # Making a new data.frame to hold matched mass features
+      Matches <- data.frame(MassFeature.X = DF.X$MassFeature.X,
+                            MassFeature.Y = NA, 
+                            mz.X = DF.X$mz.X, 
+                            mz.Y = NA,
+                            RT.X = DF.X$RT.X,
+                            RT.Y = NA,
+                            NumMatched = Matched.Y,
+                            ppm = NA, 
+                            RTdif = NA)
+      
+      Matches <- dlply(Matches, "MassFeature.X")
+      
+      # If there was more than 1 potential match and the RT was listed, take 
+      # the one that was closest by m/z and then by RT. If there was no RT 
+      # listed, take all matches. 
+      # Calculate the difference in m/z in ppm and RT in min.
+      for (i in which(Matched.Y > 0)){
             
-      } else {
-            
-            for (n in 1:nrow(MFmatch[[i]])){
-                  MFmatch[[i]]$ppm[n] <- 
-                        abs((mz.X[i] - MFmatch[[i]]$mz.Y[n])/mz.X[i]*1e6)
-                  MFmatch[[i]]$RTdif[n] <- 
-                        abs(RT.X[i] - MFmatch[[i]]$RT.Y[n])
-                  MFmatch[[i]]$MassFeature.X <- MFname.X[i]
+            if (is.na(Matches[[i]]$RT.X)){
+                  Matches[[i]] <- MFmatch[[i]]
+                  Matches[[i]]$ppm <- 
+                        abs((mz.X[i] - Matches[[i]]$mz.Y)/mz.X[i]*1e6)
+                  Matches[[i]]$MassFeature.X <- MFname.X[i]
+                  
+            } else {
+                  
+                  for (n in 1:nrow(MFmatch[[i]])){
+                        MFmatch[[i]]$ppm[n] <- 
+                              abs((mz.X[i] - MFmatch[[i]]$mz.Y[n])/mz.X[i]*1e6)
+                        MFmatch[[i]]$RTdif[n] <- 
+                              abs(RT.X[i] - MFmatch[[i]]$RT.Y[n])
+                        MFmatch[[i]]$MassFeature.X <- MFname.X[i]
+                  }
+                  MFmatch[[i]] <- arrange(MFmatch[[i]], ppm, RTdif)
+                  
+                  Matches[[i]]$MassFeature.Y <- 
+                        as.character(MFmatch[[i]]$MassFeature.Y[1])
+                  Matches[[i]]$mz.Y <- MFmatch[[i]]$mz.Y[1]
+                  Matches[[i]]$RT.Y <- MFmatch[[i]]$RT.Y[1]
+                  Matches[[i]]$ppm <- MFmatch[[i]]$ppm[1]
+                  Matches[[i]]$RTdif <- MFmatch[[i]]$RTdif[1]
+                  
             }
-            MFmatch[[i]] <- arrange(MFmatch[[i]], ppm, RTdif)
-            
-            Matches[[i]]$MassFeature.Y <- 
-                  as.character(MFmatch[[i]]$MassFeature.Y[1])
-            Matches[[i]]$mz.Y <- MFmatch[[i]]$mz.Y[1]
-            Matches[[i]]$RT.Y <- MFmatch[[i]]$RT.Y[1]
-            Matches[[i]]$ppm <- MFmatch[[i]]$ppm[1]
-            Matches[[i]]$RTdif <- MFmatch[[i]]$RTdif[1]
-            
       }
-}
-
-Matches <- rbind.fill(Matches)
-
-# Add to the Matches data.frame any MFs in Y that weren't found in X.
-NoMatch <- DF.Y[DF.Y$MassFeature %in% 
-                      setdiff(DF.Y$MassFeature, Matches$MassFeature.Y), ]
-
-Matches <- rbind.fill(Matches, NoMatch)
-Matches$mz.mean <- apply(Matches[, c("mz.X", "mz.Y")], 1, FUN = mean, 
-                         na.rm=T)
-Matches <- arrange(Matches, mz.mean)
-
-names(Matches)
-
-ColSuffix.X <- as.character(deparse(substitute(X)))
-ColSuffix.Y <- as.character(deparse(substitute(Y)))
-
-OrigNames <- names(Matches)
-
-names(Matches)[str_detect(OrigNames, "Y$")] <- sub(
-      "Y$", ColSuffix.Y, OrigNames)[str_detect(OrigNames, "Y$")]
-names(Matches)[str_detect(OrigNames, "X$")] <- sub(
-      "X$", ColSuffix.X, OrigNames)[str_detect(OrigNames, "X$")]
-
-return(Matches)
-
+      
+      Matches <- rbind.fill(Matches)
+      
+      # Add to the Matches data.frame any MFs in Y that weren't found in X.
+      NoMatch <- DF.Y[DF.Y$MassFeature %in% 
+                            setdiff(DF.Y$MassFeature, Matches$MassFeature.Y), ]
+      
+      Matches <- rbind.fill(Matches, NoMatch)
+      Matches$mz.mean <- apply(Matches[, c("mz.X", "mz.Y")], 1, FUN = mean, 
+                               na.rm=T)
+      Matches <- arrange(Matches, mz.mean)
+      
+      names(Matches)
+      
+      ColSuffix.X <- as.character(deparse(substitute(X)))
+      ColSuffix.Y <- as.character(deparse(substitute(Y)))
+      
+      OrigNames <- names(Matches)
+      
+      names(Matches)[str_detect(OrigNames, "Y$")] <- sub(
+            "Y$", ColSuffix.Y, OrigNames)[str_detect(OrigNames, "Y$")]
+      names(Matches)[str_detect(OrigNames, "X$")] <- sub(
+            "X$", ColSuffix.X, OrigNames)[str_detect(OrigNames, "X$")]
+      
+      return(Matches)
+      
 }
 
 
